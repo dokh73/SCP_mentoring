7ebe128303aa64a8668eea126d0ed6b0d50616a9
dokh73 <55347108+dokh73@users.noreply.github.com>
2019-09-30 18:15:26 +0900

    Add files via upload

---
+++ 10869.py
@@ -0,0 +1,16 @@
+def plus(a, b):
+    print(a+b)
+def minus(a,b):
+    print(a-b)
+def multi(a,b):
+    print(a*b)
+def divide(a,b):
+    print(int(a/b))
+def rest(a,b):
+    print(a%b)
+a,b=map(int,input().split())
+plus(a,b)
+minus(a,b)
+multi(a,b)
+divide(a,b)
+rest(a,b)
